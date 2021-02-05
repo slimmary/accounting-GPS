0d9130eaf084fb1f25caee88abad33f22af08597
Mary <slimmarkelova@gmail.com>
2021-02-05 18:34:20 +0200

    create plan of work orders class in workorders model

--- workorders/models.py
+++ workorders/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from datetime import date
-from clients.models import Client
+from clients.models import Client, ContactProfile
 from users.models import User
 from vehicle.models import Vehicle
 from projects.models import Project
@@ -13,6 +13,252 @@ def get_first_name(self):
 
 
 User.add_to_class("__str__", get_first_name)
+
+
+class ServicePlan(models.Model):
+    date_create = models.DateField(verbose_name='Дата',
+                                   default=date.today(),
+                                   help_text='Введіть дату',
+                                   )
+
+    class TypeService:
+        install = 'монтаж'
+        disinstall = 'демонтаж'
+        dis_install = 'демонтаж+монтаж'
+        send = 'отправка'
+        fix = 'ремонт'
+        dis_fix = 'демонтаж+ремонт'
+        install_fix = 'монтаж+ремонт'
+
+    TYPE_SERVICE_CHOICE = (
+        (TypeService.install, 'монтаж'),
+        (TypeService.disinstall, 'демонтаж'),
+        (TypeService.dis_install, 'демонтаж+монтаж'),
+        (TypeService.send, 'отправка'),
+        (TypeService.fix, 'ремонт'),
+        (TypeService.dis_fix, 'демонтаж+ремонт'),
+        (TypeService.install_fix, 'монтаж+ремонт'),
+    )
+    type_of_service = models.CharField(max_length=100,
+                                       choices=TYPE_SERVICE_CHOICE,
+                                       verbose_name='Тип робіт',
+                                       help_text='Оберіть тип',
+                                       )
+
+    class City:
+        kyiv = 'Київ'
+        vinnytsia = 'Вінниця'
+        lutsk = 'Луцьк'
+        dnipro = 'Дніпро'
+        zhytomir = 'Житомир'
+        uzhgorod = 'Ужгород'
+        zaporizhzhia = 'Запоріжжя'
+        iv_frankivsk = 'Івано-Франківськ'
+        kropivnitskiy = 'Кропивницький'
+        lviv = 'Львів'
+        mykolaiv = 'Миколаїв'
+        odessa = 'Одеса'
+        poltava = 'Полтава'
+        sumy = 'Суми'
+        rivne = 'Рівне'
+        ternopil = 'Тернопіль'
+        harkiv = 'Харків'
+        herson = 'Херсон'
+        hmelnitskiy = 'Хмельницький'
+        cherkassy = 'Черкаси'
+        chernihiv = 'Чернігів'
+        chernivtsi = 'Чернівці'
+
+    CITY_CHOICE = (
+        (City.kyiv, 'Київ'),
+        (City.vinnytsia, 'Вінниця'),
+        (City.lutsk, 'Луцьк'),
+        (City.dnipro, 'Дніпро'),
+        (City.zhytomir, 'Житомир'),
+        (City.uzhgorod, 'Ужгород'),
+        (City.zaporizhzhia, 'Запоріжжя'),
+        (City.iv_frankivsk, 'Івано-Франківськ'),
+        (City.kropivnitskiy, 'Кропивницький'),
+        (City.lviv, 'Львів'),
+        (City.mykolaiv, 'Миколаїв'),
+        (City.odessa, 'Одеса'),
+        (City.poltava, 'Полтава'),
+        (City.sumy, 'Суми'),
+        (City.rivne, 'Рівне'),
+        (City.ternopil, 'Тернопіль'),
+        (City.harkiv, 'Харків'),
+        (City.herson, 'Херсон'),
+        (City.hmelnitskiy, 'Хмельницький'),
+        (City.cherkassy, 'Черкаси'),
+        (City.chernihiv, 'Чернігів'),
+        (City.chernivtsi, 'Чернівці'),
+    )
+    city = models.CharField(max_length=100,
+                            null=True,
+                            choices=CITY_CHOICE,
+                            verbose_name='місто',
+                            help_text='Оберіть місто',
+                            blank=True
+                            )
+
+    class District:
+        kyiv = 'Київська'
+        vinnytsia = 'Вінницька'
+        volyn = 'Волинська'
+        dnipro = 'Дніпропетровьска'
+        zhytomir = 'Житомирська'
+        zakarpattya = 'Закарпатська'
+        zaporizhzhia = 'Запоріжська'
+        iv_frankivsk = 'Івано-Франківська'
+        kropivnitskiy = 'Кіровоградська'
+        lviv = 'Львівська'
+        mykolaiv = 'Миколаївська'
+        odessa = 'Одеська'
+        poltava = 'Полтавська'
+        rivne = 'Рівненська'
+        sumy = 'Сумська'
+        ternopil = 'Тернопільська'
+        harkiv = 'Харківська'
+        herson = 'Херсонська'
+        hmelnitskiy = 'Хмельницька'
+        cherkassy = 'Черкаська'
+        chernihiv = 'Чернігівська'
+        chernivtsi = 'Чернівецька'
+
+    DISTRICT_CHOICE = (
+        (District.kyiv, 'Київська'),
+        (District.vinnytsia, 'Вінницька'),
+        (District.volyn, 'Волинська'),
+        (District.dnipro, 'Дніпропетровьска'),
+        (District.zhytomir, 'Житомирська'),
+        (District.zakarpattya, 'Закарпатська'),
+        (District.zaporizhzhia, 'Запоріжська'),
+        (District.iv_frankivsk, 'Івано-Франківська'),
+        (District.kropivnitskiy, 'Кіровоградська'),
+        (District.lviv, 'Львівська'),
+        (District.mykolaiv, 'Миколаївська'),
+        (District.odessa, 'Одеська'),
+        (District.poltava, 'Полтавська'),
+        (District.rivne, 'Рівненська'),
+        (District.sumy, 'Сумська'),
+        (District.ternopil, 'Тернопільська'),
+        (District.harkiv, 'Харківська'),
+        (District.herson, 'Херсонська'),
+        (District.hmelnitskiy, 'Хмельницька'),
+        (District.cherkassy, 'Черкаська'),
+        (District.chernihiv, 'Чернігівська'),
+        (District.chernivtsi, 'Чернівецька'),
+    )
+    district = models.CharField(max_length=100,
+                                null=True,
+                                choices=DISTRICT_CHOICE,
+                                verbose_name='область',
+                                help_text='Оберіть область',
+                                blank=True
+                                )
+
+    class Delivery:
+        personal_car = 'власний автомобіль'
+        client = 'силами замовника'
+        carrier = 'перевізник'
+
+    DELIVERY_CHOICE = (
+        (Delivery.personal_car, 'власний автомобіль'),
+        (Delivery.client, 'силами замовника'),
+        (Delivery.carrier, 'перевізник'),
+    )
+
+    type_of_delivery = models.CharField(max_length=100,
+                                        null=True,
+                                        choices=DELIVERY_CHOICE,
+                                        verbose_name='Спосіб проїду',
+                                        )
+
+    tasks = models.CharField(max_length=100,
+                             null=True,
+                             verbose_name='Задачі',
+                             help_text='Опишіть перелік запланованих робіт',
+                             )
+
+    date_planing = models.DateField(verbose_name='Строк з / Дата',
+                                    help_text='Введіть дату',
+                                    )
+
+    time = models.CharField(max_length=100,
+                            verbose_name='Строк до / Час',
+                            help_text='Введіть дату або час вручну',
+                            )
+
+    client = models.ForeignKey(Client,
+                               null=True,
+                               on_delete=models.CASCADE,
+                               verbose_name='клієнт',
+                               related_name='work_orders_plan',
+                               )
+    adress = models.CharField(max_length=100,
+                              null=True,
+                              verbose_name='Адреса',
+                              )
+    contact = models.ForeignKey(ContactProfile,
+                                null=True,
+                                on_delete=models.CASCADE,
+                                verbose_name='Контактна особа',
+                                related_name='wo_plan_contact',
+                                )
+    contact_2 = models.ForeignKey(ContactProfile,
+                                  null=True,
+                                  on_delete=models.CASCADE,
+                                  verbose_name='Дод. контактна особа',
+                                  related_name='wo_plan_contact_2',
+                                  blank=True,
+                                  )
+    respons_manager = models.ForeignKey(User,
+                                        null=True,
+                                        on_delete=models.CASCADE,
+                                        verbose_name='Відповідальний менеджер',
+                                        related_name='wo_plan_response',
+                                        blank=True
+                                        )
+
+    wo_numb = models.PositiveIntegerField(null=True,
+                                          verbose_name='№',
+                                          help_text='Номер ЗН',
+                                          blank=True
+                                          )
+
+    date_ex = models.DateField(verbose_name='Дата виконання',
+                               help_text='Введіть дату',
+                               blank=True
+                               )
+
+    executor = models.ForeignKey(User,
+                                 null=True,
+                                 on_delete=models.CASCADE,
+                                 verbose_name='виконавець',
+                                 related_name='work_orders_plan_ex',
+                                 blank=True
+                                 )
+
+    class StatusWOPlan:
+        executed = 'виконано'
+        postponed = 'перенесено'
+
+    STATUSWOPLAN_CHOICE = (
+        (StatusWOPlan.executed, 'виконано'),
+        (StatusWOPlan.postponed, 'перенесено'),
+    )
+
+    status = models.CharField(max_length=100,
+                              null=True,
+                              choices=STATUSWOPLAN_CHOICE,
+                              verbose_name='Статус виконання',
+                              )
+
+    def clean(self):
+        if self.city is None and self.district is None:
+            raise ValidationError('Має бути обране або область або місто')
+        elif self.city and self.district:
+            raise ValidationError('Оберіть щось одне або область або місто')
 
 
 class WorkOrder(models.Model):
@@ -36,7 +282,7 @@ class WorkOrder(models.Model):
     type_of_work = models.CharField(max_length=100,
                                     default=TypeWork.project,
                                     choices=TYPE_WORK_CHOICE,
-                                    verbose_name='Тип ЗН (Проект/Серві',
+                                    verbose_name='Тип ЗН (Проект/Сервіc)',
                                     help_text='Оберіть тип',
 
                                     )
@@ -157,7 +403,7 @@ class WorkOrder(models.Model):
         if self.type_of_work != self.TypeWork.project and self.project:
             raise ValidationError('Не можливо приєднати проект, якщо тип ЗН не Проект')
 
-    def save(self,*args, **kwargs):
+    def save(self, *args, **kwargs):
         self.milege_price_executor = self.milege * 4.5
         if self.pay_form == self.PayForm.taxfree:
             self.milege_price_client = self.milege * 4.5
@@ -165,7 +411,7 @@ class WorkOrder(models.Model):
             self.milege_price_client = self.milege * 5.4
 
         super(WorkOrder, self).save(*args, **kwargs)
-        
+
     def __str__(self):
         return 'ЗН №{}, від {}'.format(
             self.number,
@@ -200,8 +446,10 @@ class CompletedWorks(models.Model):
                                      )
 
     used_equipment = models.ManyToManyField(Equipment,
+                                            null=True,
                                             verbose_name='використане обладнання',
-                                            related_name='used_equipment'
+                                            related_name='used_equipment',
+                                            blank=True
                                             )
 
     class Payer:
@@ -240,7 +488,7 @@ class CompletedWorks(models.Model):
                                     )
     info = models.CharField(max_length=100,
                             null=True,
-                            verbose_name='додаткова інформація з протоколу огляду',
+                            verbose_name='додаткова інформація\nз протоколу огляду',
                             blank=True
                             )
 
